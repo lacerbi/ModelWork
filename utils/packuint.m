0ecdcf3ee9e18476d54c57f07118ab073bbab6f0
Luigi Acerbi <luigi.acerbi@nyu.edu>
2016-08-14 10:02:48 -0400

    improved integer packing

--- utils/packuint.m
+++ utils/packuint.m
@@ -3,17 +3,34 @@ function c = packuint(x)
 %
 %   C = PACKUINT(X) converts a numeric array of non-negative integers to
 %   a 'packed' character array representation. The elements of X need to
-%   be non-negative integers (from 0 to 127).
+%   be non-negative integers (from 0 to 1024).
 %
 %   See also UNPACKUINT.
 
 % 21/08/2015 First version
 
-symbol = ['0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzàáâãäåæçèéêëìíîïğñòóôõöøùúûüışÿ',char(256:290)];
-
 if ~isnumeric(x); error('X needs to be a numeric array.'); end
     
-x = uint8(rmtrailzeros(x));    % Remove trailing zeros
-c = zeros(1,length(x));    
-for i = 1:length(x); c(i) = symbol(x(i)+1); end
-c = char(c);
+if 0
+    symbol = ['0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzàáâãäåæçèéêëìíîïğñòóôõöøùúûüışÿ',char(256:290)];
+    x = uint8(rmtrailzeros(x));    % Remove trailing zeros
+    c = zeros(1,length(x));    
+    for i = 1:length(x); c(i) = symbol(x(i)+1); end
+    c = char(c);
+else
+    units = ['0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+    tens = ['abcdefghijklmnopqrstuvwxyzàáâãäåæçèéêëìíîïğñòóôõöøùúûüışÿ'];
+    base = numel(units);
+    c = [];
+    x = rmtrailzeros(x);    % Remove trailing zeros
+    for i = 1:numel(x)
+        if x(i) >= base
+            c2 = mod(uint16(x(i)), base) + 1;
+            c1 = floor(x(i) / base);
+            c = [c,tens(c1),units(c2)];
+        else
+            c = [c,units(x(i)+1)];
+        end
+    end
+    c = char(c);
+end
--- utils/unpackuint.m
+++ utils/unpackuint.m
@@ -2,7 +2,7 @@ function x = unpackuint(c)
 %UNPACKUINT Convert packed string representation into integer array.
 %
 %   X = UNPACKUINT(C) converts a 'packed' character array representation 
-%   into a numeric array of non-negative integers (from 0 to 127). 
+%   into a numeric array of non-negative integers (from 0 to 1024). 
 %
 %   See also PACKUINT.
 
@@ -14,14 +14,37 @@ function x = unpackuint(c)
 
 % 21/08/2015 First version
 
-symbol = ['0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzàáâãäåæçèéêëìíîïğñòóôõöøùúûüışÿ',char(256:290)];
-
 if ~ischar(c); error('C needs to be a character array.'); end
 
-x = zeros(1,length(c));
-for i = 1:length(c)
-    index = find(c(i) == symbol,1);
-    if isempty(index); error('Unknown character is character array representation C.'); end
-    x(i) = index - 1;        
+if 0
+    symbol = ['0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzàáâãäåæçèéêëìíîïğñòóôõöøùúûüışÿ',char(256:290)];
+    x = zeros(1,length(c));
+    for i = 1:length(c)
+        index = find(c(i) == symbol,1);
+        if isempty(index); error('Unknown character in character array representation C.'); end
+        x(i) = index - 1;        
+    end
+    x = uint8(rmtrailzeros(x));    % Remove trailing zeros
+else
+    units = ['0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+    tens = ['abcdefghijklmnopqrstuvwxyzàáâãäåæçèéêëìíîïğñòóôõöøùúûüışÿ'];
+    
+    base = numel(units);
+    x = [];
+    tmp = 0;
+    for i = 1:numel(c)
+        index = find(c(i) == units,1);
+        if isempty(index)
+            index = find(c(i) == tens,1);
+            if isempty(index) || tmp > 0
+                error('Unknown character in character array representation C.');
+            end
+            tmp = base * index;
+            i = i+1;
+        else
+            x = [x, index-1 + tmp];
+            tmp = 0;
+        end        
+    end
+    x = rmtrailzeros(double(x));
 end
-x = uint8(rmtrailzeros(x));    % Remove trailing zeros
